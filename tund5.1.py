b97e27522f1e5275e58b2fcb7ff5a0f305b4f70f
denchikLOG <deniss.melnikov@gmail.com>
2025-03-27 10:58:24 +0200

    Update tund5.1.py

--- tund5.1.py
+++ tund5.1.py
@@ -1,15 +1,15 @@
 #esimene modul
 def arithmetic(arv1:float,arv2:float,tehe:str):
-    """Funktsioon töötab nagu lihtne kalkulaator
+    """Funktsioon tÃ¶Ã¶tab nagu lihtne kalkulaator
     + - liitmine
     - - lahutamine
     * - korrutamine
     / - jagamine
-    kui sisend ei ole järjendis[+,-,/,*], siis tagastab sõne "tundmatu tehe"
-    :param float arv1: sisesnd ujukomaavru tüübina
-    :param float arv2: sisesnd ujukomaavru tüübina
+    kui sisend ei ole jÃ¤rjendis[+,-,/,*], siis tagastab sÃµne "tundmatu tehe"
+    :param float arv1: sisesnd ujukomaavru tÃ¼Ã¼bina
+    :param float arv2: sisesnd ujukomaavru tÃ¼Ã¼bina
     :param str tehe: Sisend listist [+,-,/,*]
-    :rtype: varmääramata tüüp (float või str)
+    :rtype: varmÃ¤Ã¤ramata tÃ¼Ã¼p (float vÃµi str)
     """
     if tehe in ["+","-","/","*"]:
         if arv2==0 and tehe=="/":
@@ -27,3 +27,59 @@ b=float(input("arv2:"))
 t=input("tehe:")
 vastus=arithmetic(a,b,t)
 print(vastus)
+#2
+def is_year_leap(aasta:int)->bool:
+    if aasta%4==0:
+        v=True
+    else:
+        v=False
+    return v
+#3
+def square(kÃ¼lg,float)->any:
+    S=kÃ¼lg**2
+    P=4*kÃ¼lg
+    d=(2)**(1/2)*kÃ¼lg
+    return S,P,d
+#4
+def season(param:int)->str:
+    if param in [1,2,12]:
+        H="talv"
+    elif param in [3,4,5]:
+        H="kevad"
+    elif param in [6,7,8]:
+        H="suvi"
+    else:
+        H="sÃ¼gis"
+    return H
+#5
+def bank(summa:float, aastad:int)->float:
+    """kirjeldage funktsioon
+    param...
+    param...
+    rtype:...hooajanimetus
+    """
+    for aasta in range(aasta):
+        summa*=1.1
+    return summa
+from random import *
+#6
+def is_prime(a=randint(1,10000))->bool:
+    print(a)
+    v=True
+    for jagaja in range(2,a):
+        if a%jagaja==0:
+            v=False
+    return v
+#7
+def date(pÃ¤ev:int,kuu:int,aasta:int)->bool:
+    if pÃ¤ev in range(1,32) and kuu in[1,3,5,7,8,10,12] and aasta>0:
+        v=True
+    elif pÃ¤ev in range(1,30) and kuu==2 and is_year_leap(aasta):
+        v=True
+    elif pÃ¤ev in range(1,29) and kuu==2 and not is_year_leap(aasta):
+        v=True
+    if pÃ¤ev in range(1,31) and kuu in[4,6,9,11] and aasta>0:
+        v=True
+    else:
+        False
+    return v
